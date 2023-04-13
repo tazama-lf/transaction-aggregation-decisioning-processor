fa9c8f4332a3e9b56adea554a7b1cec8da181d27
Len Bekker <len.bekker@sybrin.com>
2023-04-13 10:33:16 +0200

    clustering wip

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
-import { configuration } from './config';
-import App from './app';
-import { LoggerService } from './helpers';
+import cluster from 'cluster';
 import apm from 'elastic-apm-node';
 import { Context } from 'koa';
+import os from 'os';
+import App from './app';
 import { ArangoDBService } from './clients/arango';
 import { RedisService } from './clients/redis';
+import { configuration } from './config';
+import { LoggerService } from './helpers';
 
 /*
  * Initialize the APM Logging
@@ -23,47 +25,74 @@ if (configuration.apm.active === 'true') {
 /*
  * Initialize the clients and start the server
  */
-export const app = new App();
 export const databaseClient = new ArangoDBService();
 export const cacheClient = new RedisService();
 
-/*
- * Centralized error handling
- **/
-app.on('error', handleError);
+export const runServer = (): void => {
+  const app = new App();
+  /*
+   * Centralized error handling
+   **/
+  app.on('error', handleError);
 
-function handleError(err: Error, ctx: Context): void {
-  if (ctx == null) {
-    LoggerService.error(err, undefined, 'Unhandled exception occured');
+  function handleError(err: Error, ctx: Context): void {
+    if (ctx == null) {
+      LoggerService.error(err, undefined, 'Unhandled exception occured');
+    }
   }
-}
 
-function terminate(signal: NodeJS.Signals): void {
+  function terminate(signal: NodeJS.Signals): void {
+    try {
+      app.terminate();
+    } finally {
+      LoggerService.log('App is terminated');
+      process.kill(process.pid, signal);
+    }
+  }
+
+  /*
+   * Start server
+   **/
+  if (Object.values(require.cache).filter(async (m) => m?.children.includes(module))) {
+    const server = app.listen(configuration.port, () => {
+      LoggerService.log(`API server listening on PORT ${configuration.port}`, 'execute');
+    });
+    server.on('error', handleError);
+
+    const errors = ['unhandledRejection', 'uncaughtException'];
+    errors.forEach((error) => {
+      process.on(error, handleError);
+    });
+
+    const signals: NodeJS.Signals[] = ['SIGTERM', 'SIGINT', 'SIGUSR2'];
+
+    signals.forEach((signal) => {
+      process.once(signal, () => terminate(signal));
+    });
+  }
+};
+
+const numCPUs = os.cpus().length > configuration.maxCPU ? configuration.maxCPU + 1: os.cpus().length  + 1;
+
+if (cluster.isPrimary && configuration.maxCPU !== 1) {
+  console.log(`Primary ${process.pid} is running`);
+
+  // Fork workers.
+  for (let i = 1; i < numCPUs; i++) {
+    cluster.fork();
+  }
+
+  cluster.on('exit', (worker, code, signal) => {
+    console.log(`worker ${worker.process.pid} died, starting another worker`);
+    cluster.fork();
+  });
+} else {
+  // Workers can share any TCP connection
+  // In this case it is an HTTP server
   try {
-    app.terminate();
-  } finally {
-    LoggerService.log('App is terminated');
-    process.kill(process.pid, signal);
+    runServer();
+  } catch (err) {
+    LoggerService.error(`Error while starting HTTP server on Worker ${process.pid}`, err);
   }
-}
-
-/*
- * Start server
- **/
-if (Object.values(require.cache).filter(async (m) => m?.children.includes(module))) {
-  const server = app.listen(configuration.port, () => {
-    LoggerService.log(`API server listening on PORT ${configuration.port}`, 'execute');
-  });
-  server.on('error', handleError);
-
-  const errors = ['unhandledRejection', 'uncaughtException'];
-  errors.forEach((error) => {
-    process.on(error, handleError);
-  });
-
-  const signals: NodeJS.Signals[] = ['SIGTERM', 'SIGINT', 'SIGUSR2'];
-
-  signals.forEach((signal) => {
-    process.once(signal, () => terminate(signal));
-  });
+  console.log(`Worker ${process.pid} started`);
 }
