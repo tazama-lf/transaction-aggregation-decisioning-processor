026e581c58b5eb90b6cbc49901a96fb99a20fab1
rtkay123 <rodney.kanjala@sybrin.com>
2023-08-11 11:56:18 +0200

    chore: update code cov

--- jest.config.ts
+++ jest.config.ts
@@ -56,7 +56,7 @@ const config: Config.InitialOptions = {
   // An object that configures minimum threshold enforcement for coverage results
   coverageThreshold: {
     global: {
-      branches: 70,
+      branches: 60,
       functions: 95,
       lines: 95,
       statements: 95,
